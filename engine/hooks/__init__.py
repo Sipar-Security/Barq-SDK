a515b37b945d677e441cf3acf4058789f55f2bed
qaziawais <kajubadaampistaqazi@gmail.com>
2026-09-08 03:16:24 +0500

    add: hook subpackage exports and initialization

---
+++ engine/hooks/__init__.py
@@ -0,0 +1,12 @@
+from .events import HookEvent, HOOK_EVENTS
+from .engine import HookEngine, HookInput, HookOutcome, FunctionHook, CommandHook
+
+__all__ = [
+    "HookEvent",
+    "HOOK_EVENTS",
+    "HookEngine",
+    "HookInput",
+    "HookOutcome",
+    "FunctionHook",
+    "CommandHook",
+]
